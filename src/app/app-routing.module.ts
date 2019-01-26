646d488c7174f3f10527f128f2c3bc4f40f9fe63
Carlos <jcarlosgarcial@outlook.com>
2019-01-26 00:04:50 -0300

    se agrega componente de acceso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccessComponent } from './components/user/access/access.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"/user/access",component:AccessComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessComponent } from './components/user/access/access.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent
   ],
   imports: [
     BrowserModule,
